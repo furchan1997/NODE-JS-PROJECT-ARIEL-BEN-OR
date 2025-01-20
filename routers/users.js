2da950b3fa47cc98500c8c7fc5abbe49f1eb205c
ariel <arielhodaya@gmail.com>
2025-01-20 07:10:19 +0200

    .

--- routers/users.js
+++ routers/users.js
@@ -13,192 +13,225 @@ const userFields = require("../data/userFields");
 const user = require("../initialDate/userAdmin");
 
 // כניסה לחשבון היוזר על ידיי מנהל או בעל החשבון באמצעות הטוקן
-router.get("/:id", authMW, checkAuthorization, async (req, res) => {
-  // בדיקת קיומו של יוזר
-  const user = await User.findOne({ _id: req.params.id });
+router.get("/:id", authMW, checkAuthorization, async (req, res, next) => {
+  try {
+    // בדיקת קיומו של יוזר
+    const user = await User.findOne({ _id: req.params.id });
 
-  if (!user) {
-    res
-      .status(errors.USERS_NOT_FOUND.status)
-      .json({ message: errors.USERS_NOT_FOUND.message });
-    return;
+    if (!user) {
+      return res
+        .status(errors.USERS_NOT_FOUND.status)
+        .json({ message: errors.USERS_NOT_FOUND.message });
+    }
+
+    res.json(picker(user, userFields));
+  } catch (err) {
+    next(err); // מעביר את השגיאה למידלוואר של טיפול בשגיאות
   }
-
-  res.json(picker(user, userFields));
 });
 
 // מחיקת משתמש קיים
-router.delete("/:id", authMW, checkAuthorization, async (req, res) => {
-  // מציאת יוזר ובדיקת קיומו
-  const user = await User.findOneAndDelete({ _id: req.params.id });
-  if (!user) {
-    res
-      .status(errors.USERS_NOT_FOUND.status)
-      .json({ message: errors.USERS_NOT_FOUND.message });
-    return;
+router.delete("/:id", authMW, checkAuthorization, async (req, res, next) => {
+  try {
+    // מציאת יוזר ובדיקת קיומו
+    const user = await User.findOneAndDelete({ _id: req.params.id });
+    if (!user) {
+      res
+        .status(errors.USERS_NOT_FOUND.status)
+        .json({ message: errors.USERS_NOT_FOUND.message });
+      return;
+    }
+
+    // אם יוזר עיסקי נמחק אז נמחקים לו כל הכרטיסים (במידה ויש לו)
+    await Card.deleteMany({ user_id: req.params.id });
+
+    const response = {
+      message: "User deleted.",
+      ...picker(user, userFields),
+    };
+
+    res.json(response);
+  } catch (err) {
+    next(err); // מעביר את השגיאה למידלוואר של טיפול בשגיאות
   }
-
-  // אם יוזר עיסקי נמחק אז נמחקים לו כל הכרטיסים (במידה ויש לו)
-  await Card.deleteMany({ user_id: req.params.id });
-
-  const response = {
-    message: "User deleted.",
-    ...picker(user, userFields),
-  };
-
-  res.json(response);
 });
 
 // עריכת פרטי משתמש קיים
-router.put("/:id", authMW, async (req, res) => {
-  // ולידציית ג'וי שמטפלת בסכמה הרלוונטית לבקשה
-  const { error } = validateUser(req.body, "PUT");
-  // בדיקה ולידציה של ג'וי
-  if (error) {
-    res.status(errors.BAD_REQUEST.status).json({
-      message: errors.BAD_REQUEST.message,
-      reason: error.details[0].message,
-    });
-    return;
-  }
-
-  // בדיקת הרשאות
-  if (req.params.id !== req.user.id) {
-    res
-      .status(403)
-      .send("Only the account owner has permission to change their details.");
-    return;
-  }
-  // עידכון המשתמש
-  const user = await User.findOneAndUpdate(
-    { _id: req.params.id },
-    {
-      ...req.body,
-    },
-    {
-      returnDocument: "after",
+router.put("/:id", authMW, async (req, res, next) => {
+  try {
+    // ולידציית ג'וי שמטפלת בסכמה הרלוונטית לבקשה
+    const { error } = validateUser(req.body, "PUT");
+    // בדיקה ולידציה של ג'וי
+    if (error) {
+      res.status(errors.BAD_REQUEST.status).json({
+        message: errors.BAD_REQUEST.message,
+        reason: error.details[0].message,
+      });
+      return;
     }
-  );
 
-  if (!user) {
-    res
-      .status(errors.USERS_NOT_FOUND.status)
-      .json({ message: errors.USERS_NOT_FOUND.message });
-    return;
+    // בדיקת הרשאות
+    if (req.params.id !== req.user.id) {
+      res
+        .status(403)
+        .send("Only the account owner has permission to change their details.");
+      return;
+    }
+
+    // עידכון המשתמש
+    const user = await User.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        ...req.body,
+      },
+      {
+        returnDocument: "after",
+      }
+    );
+
+    if (!user) {
+      res
+        .status(errors.USERS_NOT_FOUND.status)
+        .json({ message: errors.USERS_NOT_FOUND.message });
+      return;
+    }
+
+    res.json(picker(user, userFields));
+  } catch (err) {
+    next(err); // מעביר את השגיאה למידלוואר של טיפול בשגיאות
   }
-  res.json(picker(user, userFields));
 });
 
 // עדכון סטטוס עיסקי
-router.patch("/:id", authMW, async (req, res) => {
-  // חיפוש משתמש לפי מזהה
-  const user = await User.findById(req.params.id);
-  // בדיקת משתמש קיים
-  if (!user) {
-    res
-      .status(errors.USERS_NOT_FOUND.status)
-      .send(errors.USERS_NOT_FOUND.message);
-    return;
-  }
-  // בדיקת הרשאות
-  if (req.user.id !== req.params.id) {
-    res
-      .status(errors.FORBIDDEN_USER.status)
-      .send(errors.FORBIDDEN_USER.message);
-    return;
-  }
+router.patch("/:id", authMW, async (req, res, next) => {
+  try {
+    // חיפוש משתמש לפי מזהה
+    const user = await User.findById(req.params.id);
+    // בדיקת משתמש קיים
+    if (!user) {
+      res
+        .status(errors.USERS_NOT_FOUND.status)
+        .send(errors.USERS_NOT_FOUND.message);
+      return;
+    }
+    // בדיקת הרשאות
+    if (req.user.id !== req.params.id) {
+      res
+        .status(errors.FORBIDDEN_USER.status)
+        .send(errors.FORBIDDEN_USER.message);
+      return;
+    }
 
-  user.isBusiness = !user.isBusiness; // משנה את הסטטוס
-  await user.save(); // שומר את השינויים במסד הנתונים
-  // טוקן חדש
-  const token = createToken({
-    id: user._id,
-    biz: user.isBusiness,
-    admin: user.isAdmin,
-    key: config.jwtkey,
-  });
-  // התגובה ללקוח
-  const response = {
-    ...picker(user, userFields),
-    message: "User status updated successfully. Please log in again.",
-    token,
-  };
+    user.isBusiness = !user.isBusiness; // משנה את הסטטוס
+    await user.save(); // שומר את השינויים במסד הנתונים
+    // טוקן חדש
+    const token = createToken({
+      id: user._id,
+      biz: user.isBusiness,
+      admin: user.isAdmin,
+      key: config.jwtkey,
+    });
+    // התגובה ללקוח
+    const response = {
+      ...picker(user, userFields),
+      message: "User status updated successfully. Please log in again.",
+      token,
+    };
 
-  res.json(response);
+    res.json(response);
+  } catch (err) {
+    next(err); // מעביר את השגיאה למידלוואר של טיפול בשגיאות
+  }
 });
 
 // יצירת משתמש חדש ושמירתו בדאטא בייס
-router.post("/", async (req, res) => {
-  // ולידציית ג'וי שמטפלת בסכמה הרלוונטית לבקשה
-  const { error } = validateUser(req.body, "POST");
-  // VALIDATE INPUT
-  if (error) {
-    res.status(errors.BAD_REQUEST.status).json({
-      message: errors.BAD_REQUEST.message,
-      reason: error.details[0].message,
+router.post("/", async (req, res, next) => {
+  try {
+    // ולידציית ג'וי שמטפלת בסכמה הרלוונטית לבקשה
+    const { error } = validateUser(req.body, "POST");
+    // VALIDATE INPUT
+    if (error) {
+      res.status(errors.BAD_REQUEST.status).json({
+        message: errors.BAD_REQUEST.message,
+        reason: error.details[0].message,
+      });
+      return;
+    }
+
+    // VALIDATE SYSTEM
+    // בדיקה אם אימייל קיים במערכת
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      res.status(errors.BAD_REQUEST.status).send({ message: "Email is busy." });
+      return;
+    }
+
+    // האשינג לסיסמא
+    const userPassword = await bcrypt.hash(req.body.password, 12);
+    // יצירת יוזר חדש ושמירתו בביס הנתונים
+    user = await new User({
+      ...req.body,
+      url:
+        req.body.image.url ??
+        "https://mitmachim.top/assets/uploads/files/1617890002090-024a6e13-918d-4a6a-b67e-f8254a5ff97b-image.png",
+      password: userPassword,
+      isAdmin: req.body.isAdmin || false,
     });
-    return;
-  }
+    await user.save();
 
-  //   VALIDATE SYSTEM
-  // בדיקה אם אימייל קיים במערכת
-  let user = await User.findOne({ email: req.body.email });
-  if (user) {
-    res.status(errors.BAD_REQUEST.status).send({ message: "Email is busy." });
-    return;
+    // RESPONSE
+    res.json(picker(user, userFields));
+  } catch (err) {
+    next(err); // מעביר את השגיאה למידלוואר של טיפול בשגיאות
   }
-  // האשינג לסיסמא
-  const userPassword = await bcrypt.hash(req.body.password, 12);
-  // יצירת יוזר חדש ושמירתו בביס הנתונים
-  user = await new User({
-    ...req.body,
-    url:
-      req.body.image.url ??
-      "https://mitmachim.top/assets/uploads/files/1617890002090-024a6e13-918d-4a6a-b67e-f8254a5ff97b-image.png",
-    password: userPassword,
-    isAdmin: req.body.isAdmin || false,
-  });
-  await user.save();
-
-  // RESPONSE
-  res.json(picker(user, userFields));
 });
 
 // קבלת כל היוזרים
-router.get("/", authMW, async (req, res) => {
-  // שאילתא עבור שליפת כל המשתמשים
-  const users = await User.find({}, {});
+router.get("/", authMW, async (req, res, next) => {
+  try {
+    // שאילתא עבור שליפת כל המשתמשים
+    const users = await User.find({}, {});
 
-  if (!users) {
-    res.status(errors.USERS_NOT_FOUND.status).json({
-      message: errors.USERS_NOT_FOUND.message,
-    });
-    return;
-  }
-  // בדיקת הרשאות
-  if (req.user.admin !== true) {
-    res
-      .status(403)
-      .send("This user does not have permission to receive all users.");
-    return;
-  }
+    if (!users) {
+      res.status(errors.USERS_NOT_FOUND.status).json({
+        message: errors.USERS_NOT_FOUND.message,
+      });
+      return;
+    }
 
-  res.json(picker(users, userFields));
+    // בדיקת הרשאות
+    if (req.user.admin !== true) {
+      res
+        .status(403)
+        .send("This user does not have permission to receive all users.");
+      return;
+    }
+
+    res.json(picker(users, userFields));
+  } catch (err) {
+    next(err); // מעביר את השגיאה למידלוואר של טיפול בשגיאות
+  }
 });
 
 // מחיקת יוזרים ואיפוס המערכת לצורך נוחות
-router.delete("/", authMW, async (req, res) => {
-  // בדיקת הרשאות
-  if (!req.user.admin) {
-    res.status(403).send("Only admin");
-    return;
-  }
-  // מחיקת יוזרים
-  await User.deleteMany({}, {});
-  // מחיקת כרטיסים
-  await Card.deleteMany({}, {});
+router.delete("/", authMW, async (req, res, next) => {
+  try {
+    // בדיקת הרשאות
+    if (!req.user.admin) {
+      res.status(403).send("Only admin");
+      return;
+    }
 
-  res.send("deleted");
+    // מחיקת יוזרים
+    await User.deleteMany({}, {});
+
+    // מחיקת כרטיסים
+    await Card.deleteMany({}, {});
+
+    res.send("deleted");
+  } catch (err) {
+    next(err); // מעביר את השגיאה למידלוואר של טיפול בשגיאות
+  }
 });
 
 module.exports = router;
